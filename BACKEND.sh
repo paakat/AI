544cdcb6e9afdfbfac51f1159bf0cc6a74b096f9
jmake <zvl_2006@hotmail.com>
2024-02-08 17:06:28 +0100

    Add files via upload

---
+++ BACKEND.sh
@@ -0,0 +1,17 @@
+##
+## conda activate /Users/poderozita/z2024_1/AI/IA1
+##
+## https://elevenlabs.io/
+## https://console.deepgram.com/project/  
+## https://platform.openai.com/usage  
+## 
+
+export OPENAI_API_KEY=
+export ELEVEN_API_KEY=
+export DEEPGRAM_API_KEY=
+
+
+python3 main.py 
+
+
+ 
